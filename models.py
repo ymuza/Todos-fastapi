e4f395adb8eddeb5fe2867b3601facaa9cf46ff3
Yamil <yamil.muza@globant.com>
2023-02-27 15:51:43 -0300

    pylint fixes

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Users(Base):
     """Users table"""
 
